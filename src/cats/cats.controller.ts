13bf65576d4f1e9043a744ce5a0f612feef3f7c2
golden821man <goldenman1991821@gmail.com>
2023-03-02 18:27:25 +0900

    Cat changed

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,13 +9,19 @@ export class CatsController {
   // // create(): string {
   // //   return 'This action adds a new cat';
   // // }
+  @Get('')
+  hello(@Param() params): string {
+    return "hello";
+  }
+  
+  @Get('ab*cd')
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats'
+  }
+
   @Get(':id')
   findOne(@Param() params): string {
     console.log(params.id);
     return `This action returns a #${params.id} cat`;
-  }
-  @Get('ab*cd')
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats'
   }
 }
